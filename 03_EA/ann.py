f42d409851ae2024071ccd497517dbfaf0cb6dbf
Glauco <glauco.lorenzut@gmail.com>
2020-02-28 21:39:38 +0100

    added forward propagation

--- 03_EA/ann.py
+++ 03_EA/ann.py
@@ -1,24 +1,28 @@
 import numpy as np
+from copy import deepcopy
 import matplotlib.pyplot as plt
 
+#np.random.randn(self.inputSize + 1, self.hiddenSize)
 
 class ArtificialNeuralNetwork:
+    # create a 2 layers NN
+    def __init__(self, n_inputs, n_hidden, n_outputs):
+        self.inputSize = n_inputs
+        self.hiddenSize = n_hidden
+        self.outputSize = n_outputs
+        self.weights_1L = np.zeros((n_inputs, n_hidden))
+        self.weights_2L = np.zeros((n_hidden, n_outputs))
+        self.bias_1L = np.ones((1, n_hidden))
+        self.bias_2L = np.ones((1, n_outputs))
 
-    def __init__(self, inputLayer, hiddenLayer, outputLayer):
-        self.inputSize = inputLayer
-        self.hiddenSize = hiddenLayer
-        self.outputSize = outputLayer
-        self.weights1 = np.random.randn(self.inputSize + 1,
-                                        self.hiddenSize)  # Random weights at the beginning (One more for the bias)
-        self.weights2 = np.random.randn(self.hiddenSize + 1,
-                                        self.outputSize)  # Random weights at the beginning (One more for the bias)
-        self.bias1 = 1
-        self.bias2 = 1
-        self.extraoutput_backprop = 1
+
+    def _random_weights(self, n, m):
+        return np.random.randn(n + 1, m)
+
 
     # Activation function
     def _bias_vector(self, length):
-        spam = np.array([[self.bias]] * length)
+        spam = np.array([[self.bias_1L]] * length)
         print("_bias_vector", spam)
         return spam
 
@@ -26,26 +30,30 @@ class ArtificialNeuralNetwork:
         return 1 / (1 + np.exp(-x))
 
     # Forward propagation
-    def forwardprop(self, x):
-        x = np.concatenate((np.array([[self.bias1]] * len(x)), x), axis=1)  # Adding the first bias (input)
-        self.hiddenLayer = self._sigmoid(np.dot(x, self.weights1))  # HiddenLayerActivation => Input x Weights1
-        self.hiddenLayer = np.concatenate((np.array([[self.bias2]] * len(self.hiddenLayer)), self.hiddenLayer),
-                                          axis=1)  # Adding the second bias (Hidden layer)
-        output = self._sigmoid(np.dot(self.hiddenLayer, self.weights2))  # Output => HiddenLayerActivation x Weights2
-        return output
+    def _forward_propagation(self, inputs):
+        hidden_layer = self._sigmoid(np.dot(inputs, self.weights_1L))
+        outputs = self._sigmoid(np.dot(hidden_layer, self.weights_2L))
+        return outputs
 
-    # Training weights
-    def train(self, x, n_epochs):
-        errors = []
-        for i in range(0, n_epochs):
-            output = self.forwardprop(x)
-            errors.append([np.square(np.subtract(x, output)).mean(), i])
+    # launch individual "life"
+    def start_individual(self, inputs, steps, output_range):
+        #errors = []
+        for i in range(steps):
+            outputs = self._forward_propagation(inputs)
+            # TODO rimettere xy nella forma normale
+            #from numpy import interp
 
-        errors = np.array(errors)
-        plt.plot(errors[:, 1], errors[:, 0])
-        plt.xlabel('Mean square error')
-        plt.ylabel('Number epochs')
-        plt.show()
+            outputs[0] = np.interp(outputs[0], [0, 1], [output_range[0], output_range[1]])
+            outputs[1] = np.interp(outputs[1], [0, 1], [output_range[0], output_range[1]])
+            inputs = outputs
+            #errors.append([np.square(np.subtract(x, output)).mean(), i])
+
+        return outputs
+        #errors = np.array(errors)
+        #plt.plot(errors[:, 1], errors[:, 0])
+        #plt.xlabel('Mean square error')
+        #plt.ylabel('Number epochs')
+        #plt.show()
 
 
 # Creation dataset
@@ -62,10 +70,14 @@ dataset = np.array(
     ]
 )
 
-train_data = dataset  # All
+train_data = deepcopy(dataset)  # All
 
 # Implementation
-ANN = ArtificialNeuralNetwork(inputLayer=8, hiddenLayer=3, outputLayer=8)
+ANN = ArtificialNeuralNetwork(2, 4, 2)
 
+print(ANN.weights_1L)
+print(ANN.weights_2L)
+print(ANN.bias_1L)
+print(ANN.bias_2L)
 # Training
-ANN.train(train_data, 1000)
+#ANN.start_individual(train_data, 1000)
